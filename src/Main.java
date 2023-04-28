15655755933c9d6e78d11265eb42ce5da350067f
Constant <110848773+joaopconstant@users.noreply.github.com>
2023-04-27 22:17:35 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        Partida partida = new Partida("Partida de Futebol", "28/04/2023 19:00", "Estádio do Pacaembu", 100, 50);
+        Partida partida = new Partida("Flamengo X Vasco", "28/04/2023 19:00", "Maracanã", 100, 50);
         System.out.println("Bem-vindo ao sistema de venda de ingressos!");
+        Ingresso ingresso = null;
 
+        // menu principal do aplicativo
         while(true) {
             System.out.println("\nDigite uma opção:");
             System.out.println("1 - Cadastrar nova partida");
@@ -31,7 +33,7 @@ public class Main {
                 int ingressosInteira = scanner.nextInt();
                 System.out.println("Insira o número de Ingressos tipo Meia disponíveis: ");
                 int ingressosMeia = scanner.nextInt();
-                
+
                 partida = new Partida(nome, data, local, ingressosMeia, ingressosInteira);
                 System.out.println("Partida cadastrada com sucesso!");
                 break;
@@ -44,27 +46,30 @@ public class Main {
                 int numero = scanner.nextInt();
                 System.out.println("Informe o tipo do Ingresso (Inteira - I / Meia - M): ");
                 char tipoIngresso = scanner.next().charAt(0);
-                TipoIngresso tipo;
+                
                 // verifica o tipo do ingresso
-                if (tipoIngresso == 'I') {
+                TipoIngresso tipo;
+                if (tipoIngresso == 'I' || tipoIngresso == 'i') {
                     tipo = TipoIngresso.INTEIRA;
-                } else if (tipoIngresso == 'M') {
+                } else if (tipoIngresso == 'M' || tipoIngresso == 'm') {
                     tipo = TipoIngresso.MEIA;
                 } else {
                     System.out.println("Tipo de Ingresso inválido!");
                     break;
                 }
+
                 System.out.println("Informe a quantidade de ingressos desejada: ");
                 int quantidade = scanner.nextInt();
+                
+                // confirma ou cancela a compra do ingresso
                 System.out.println("Confirma a compra do ingresso? (S/N)");
                 char confirmacao = scanner.next().charAt(0);
-                if (confirmacao == 'S') {
-                // verifica se o ingresso está disponível
+                if (confirmacao == 'S' || confirmacao == 's') {
+                    // verifica se o ingresso está disponível
                     if (partida.isIngressoDisponivel(tipo, quantidade)) {
-                // gera o ingresso e exibe as informações
+                    // gera o ingresso e exibe as informações
                         Assento assento = new Assento(fila, numero);
                         double preco = tipo == TipoIngresso.INTEIRA ? 50.0 : 25.0;
-                        Ingresso ingresso;
                         if (tipo == TipoIngresso.INTEIRA) {
                             ingresso = new IngressoInteira(partida, assento, preco);
                         } else {
@@ -72,12 +77,21 @@ public class Main {
                         }
                         System.out.println("Ingresso gerado:");
                         System.out.println(ingresso);
-                // realiza a venda do ingresso
+                        // realiza a venda do ingresso
                         double valorVenda = partida.venderIngresso(tipo, quantidade);
                         System.out.println(String.format("Ingresso vendido por R$%.2f.", valorVenda));
-                    } else {
+                    } 
+                    
+                    else {
                         System.out.println("Ingressos esgotados para o tipo selecionado.");
                     }
+                } 
+                
+                // cancela a compra
+                else if (confirmacao == 'N' || confirmacao == 'n') {
+                    System.out.println("Compra cancelada! Voltando para o Menu...");
+                } else {
+                    System.out.println("Dado inválido. Tente novamente.");
                 }
                 break;
 
@@ -89,20 +103,38 @@ public class Main {
                 break;
 
                 case 4:
+                // exibe o número de ingressos restantes
                 System.out.println("-----------------------------------");
                 System.out.println("Número de ingressos restantes: " + partida.getIngressos());
                 System.out.println("-----------------------------------");
                 break;
 
                 case 5:
+                // verifica e exibe as informações da ultima venda
+                if (ingresso != null) {
+                    System.out.println("-----------------------------------");
+                    System.out.println("Informações do último ingresso vendido:");
+                    System.out.println("-----------------------------------");
+                    System.out.println(ingresso);
+                } else {
+                    System.out.println("-----------------------------------");
+                    System.out.println("Não houve nenhuma venda até o momento.");
+                    System.out.println("-----------------------------------");
+                }
                 break;
                 
                 case 6:
+                // printa a mensagem de saída
                 System.out.println("Saindo...");
                 break;
 
                 default:
                 System.out.println("Opção inválida! Tente novamente.");
+            }
+
+            // fecha definitivamente o sistema
+            if (opcao == 6) {
+                break;
             }
         }
     }
